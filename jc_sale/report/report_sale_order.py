b82c33303599f8c37941df3bd0eac46fe82010aa
Eric <xxbregister@126.com>
2017-09-18 14:25:28 +0800

    报表添加字段；默认为tree

--- jc_sale/report/report_sale_order.py
+++ jc_sale/report/report_sale_order.py
@@ -25,30 +25,11 @@ class ReportSaleOrder(models.Model):
     goods_id = fields.Many2one('archives.goods', string=u'产品')
     second_unit_id = fields.Many2one('archives.unit', string=u'辅单位')
     second_unit_number = fields.Float(digits=dp.get_precision('Quantity'), string=u'辅数量')
-    main_unit_id = fields.Many2one('archives.unit', string=u'主单位', compute='_set_main')
+    main_unit_id = fields.Many2one('archives.unit', string=u'主单位')
     main_unit_number = fields.Float(digits=dp.get_precision('Quantity'), string=u'主数量')
     price = fields.Float(u'单价', digits=dp.get_precision('Price'))
     money = fields.Float(digits=dp.get_precision('Amount'), string=u'金额')
     remark_detail = fields.Char(string=u'备注')
-
-    # department_id = fields.Many2one('staff.department', u'部门')
-    # user_id = fields.Many2one('res.users', u'销售员')
-    # goods = fields.Char(u'商品名')
-    # goods_id = fields.Many2one('goods', u'商品')
-    # brand_id = fields.Many2one('core.value', u'品牌')
-    # location = fields.Char(u'库位')
-    # uom = fields.Char(u'单位')
-    # uos = fields.Char(u'辅助单位')
-    # lot = fields.Char(u'批号')
-    # attribute_id = fields.Char(u'属性')
-    # warehouse = fields.Char(u'仓库')
-    # goods_qty = fields.Float(u'数量', digits=dp.get_precision('Quantity'))
-    # goods_uos_qty = fields.Float(u'辅助单位数量', digits=dp.get_precision('Quantity'))
-    # price = fields.Float(u'单价', digits=dp.get_precision('Price'))
-    # amount = fields.Float(u'销售收入', digits=dp.get_precision('Amount'))
-    # tax_amount = fields.Float(u'税额', digits=dp.get_precision('Amount'))
-    # subtotal = fields.Float(u'价税合计', digits=dp.get_precision('Amount'))
-    # margin = fields.Float(u'毛利', digits=dp.get_precision('Amount'))
 
     @api.multi
     def test(self):
@@ -71,16 +52,17 @@ b.company_id,
 b.staff_id,
 b.store_id,
 d.goods_id,
---d.second_unit_id,
+g.second_unit_id,
 sum(d.second_unit_number) second_unit_number,
---d.main_unit_id,
+g.main_unit_id,
 sum(d.main_unit_number) main_unit_number,
 d.price,
---sum(d.money),
+sum(d.money) money,
 d.remark remark_detail
 FROM jc_sale_sale_order b 
 LEFT JOIN jc_sale_sale_order_detail d ON d.sale_order_id = b.id
-group by b.id,b.bill_state,b.customer_id,b.date,b.sale_type_id,b.remark,b.company_id,b.staff_id,b.store_id,d.goods_id,d.price,d.remark
+LEFT JOIN archives_goods g on d.goods_id = g.id
+group by b.id,b.bill_state,b.customer_id,b.date,b.sale_type_id,b.remark,b.company_id,b.staff_id,b.store_id,d.goods_id,g.second_unit_id,g.main_unit_id,d.price,d.remark
 
             )
         """)
