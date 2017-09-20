dbbb8e1b99b90aad8043121ad19c72823566edf4
Eric <xxbregister@126.com>
2017-09-20 21:13:13 +0800

    优化部门： 1.上级部门，只能选择同公司中的除了自己之外的部门 2.上级部门、公司，不允许新建

--- archives/models/department.py
+++ archives/models/department.py
@@ -7,30 +7,25 @@ from odoo import tools, _
 from odoo.exceptions import ValidationError
 from odoo.modules.module import get_module_resource
 
+
 class Department(models.Model):
     _name = 'archives.department'
     _description = u'档案：部门'
 
     _sql_constraints = [
-       ('name_unique',
-        'UNIQUE(name)',
-        "已存在同名部门"),
-   ]
+        ('name_unique',
+         'UNIQUE(name)',
+         "已存在同名部门"),
+    ]
 
-    name = fields.Char(string=u'部门名称',required=True)
-    active=fields.Boolean('Active',default=True)
-    company_id=fields.Many2one('res.company',string=u'公司',index=True)#,default=lambda self:self.evn.user.company_id
-    parent_id=fields.Many2one('archives.department',string=u'上级部门',index=True)
-    #child_ids=fields.One2many('archive.department',string=u'子部门')
+    name = fields.Char(string=u'部门名称', required=True)
+    active = fields.Boolean('Active', default=True)
+    company_id = fields.Many2one('res.company', string=u'公司',
+                                 index=True)  # ,default=lambda self:self.evn.user.company_id
+    parent_id = fields.Many2one('archives.department', string=u'上级部门', index=True,
+                                domain="[('company_id', '=', company_id), ('id', '!=', id)]")
 
     @api.constrains('parent_id')
     def _check_parent_id(self):
         if not self._check_recursion():
             raise ValidationError(_(u'错误!与上级部门互为上下级，发生循环.'))
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
