b579192195b73200d11e1a07b648746795c68943
Eric <xxbregister@126.com>
2017-10-10 16:34:39 +0800

    用户加首拼，升级报错（还是登录就报错？）。 应该是因为用户是一开始就使用的，修改后，直接就用上了，但是要完全用上，需要升级，而登录时，根本不能升级。这样的问题，如何解决？

--- archives/models/res_user.py
+++ archives/models/res_user.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from odoo import _, api, exceptions, fields, models
+# from . import utils
 
 
 class Users(models.Model):
     _name = 'res.users'
     _inherit = ['res.users']
+
+    # spell = fields.Char(string=u'首拼')
 
     organization_id = fields.Many2one('archives.organization', string=u'权限单据ID', ondelete='cascade', copy=False)
     # active_customer_staff = fields.Boolean('启用客户销售人员权限')
@@ -13,3 +16,13 @@ class Users(models.Model):
     #
     # organization_customer_staff = fields.One2many('res.users.organization_customer_staff', 'user_id',
     #                                               string=u'数据权限_客户销售人员权限明细', copy=True)
+
+    # @api.model
+    # def create(self, values):
+    #     utils.set_spell(values)
+    #     return super(Users, self).create(values)
+    #
+    # @api.multi
+    # def write(self, values):
+    #     utils.set_spell(values)
+    #     return super(Users, self).write(values)
