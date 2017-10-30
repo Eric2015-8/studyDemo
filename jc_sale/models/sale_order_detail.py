126e69843262e917b85563d00866252fc568b0e9
Eric <xxbregister@126.com>
2017-10-30 18:04:12 +0800

    优化手机添加订单明细的操作：数字字段（金额除外）添加对于的辅助字段，并显示在界面上，同时隐藏数字字段

--- jc_sale/models/sale_order_detail.py
+++ jc_sale/models/sale_order_detail.py
@@ -20,10 +20,13 @@ class SaleOrderDetail(models.Model):
     goods_id = fields.Many2one('archives.goods', string=u'产品', required=True)
     second_unit_id = fields.Many2one('archives.unit', string=u'辅单位', compute='_set_second', store=True)
     second_unit_number = fields.Float(digits=(6, 2), string=u'辅数量')
+    second_unit_number_tmp = fields.Char(string=u'辅数量')
     main_unit_id = fields.Many2one('archives.unit', string=u'主单位', compute='_set_main', store=True)
     main_unit_number = fields.Float(digits=(6, 2), string=u'主数量')
+    main_unit_number_tmp = fields.Char(string=u'主数量')
 
     price = fields.Float(digits=(6, 2), help="单价", string=u'单价')
+    price_tmp = fields.Char(string=u'单价')
     money = fields.Float(digits=(6, 2), help="金额", string=u'金额', store=True)
 
     remark = fields.Char(string=u'备注')
@@ -37,6 +40,30 @@ class SaleOrderDetail(models.Model):
     def _set_second(self):
         for record in self:
             record.second_unit_id = record.goods_id.second_unit_id
+
+    @api.onchange('second_unit_number_tmp')
+    def _onchange_for_second_unit_number_from_tmp(self):
+        self.second_unit_number = float(self.second_unit_number_tmp)
+        if not self.goods_id.need_second_change:
+            return
+        self.main_unit_number = self.goods_id.second_rate * self.second_unit_number
+        self.main_unit_number_tmp = str(self.main_unit_number)
+        self.money = self.price * self.main_unit_number
+
+    @api.onchange('main_unit_number_tmp')
+    def _onchange_for_main_unit_number_from_tmp(self):
+        self.main_unit_number = float(self.main_unit_number_tmp)
+        self.money = self.price * self.main_unit_number
+        if not self.goods_id.need_second_change:
+            return
+        if self.goods_id.second_rate != 0:
+            self.second_unit_number = self.main_unit_number / self.goods_id.second_rate
+            self.second_unit_number_tmp = str(self.second_unit_number)
+
+    @api.onchange('price_tmp')
+    def _onchange_for_price_from_tmp(self):
+        self.price = float(self.price_tmp)
+        self.money = self.price * self.main_unit_number
 
     @api.onchange('price', 'main_unit_number')
     def _onchange_for_money(self):
