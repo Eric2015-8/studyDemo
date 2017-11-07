13645d4ec8147da1e282a6b64ed913ea8ff56ae8
Eric <xxbregister@126.com>
2017-11-07 11:55:41 +0800

    修改销售类型的设置：由1个设置项改为3个设置项

--- jc_sale/models/sale_setting_center_sale_type.py
+++ jc_sale/models/sale_setting_center_sale_type.py
@@ -14,7 +14,9 @@ class SaleSettingCenterSaleType(models.Model):
 
     sale_type_id = fields.Many2one('archives.common_archive', string=u'销售类型', required=True,
                                    domain=[('archive_name', '=', 1)])
-    type = fields.Selection(create_type, default=10, string=u'生成方式')
+    forecast_2_oder_type = fields.Selection(create_type, default=1, string=u'预报生成订单方式', required=True)
+    order_2_out_store_type = fields.Selection(create_type, default=10, string=u'订单生成出库方式', required=True)
+    out_store_2_account_type = fields.Selection(create_type, default=10, string=u'出库生成账单方式', required=True)
 
     def _set_name(self, values):
         values['name'] = self.env['archives.common_archive'].browse(values['sale_type_id']).name
@@ -31,3 +33,15 @@ class SaleSettingCenterSaleType(models.Model):
         if count:
             raise ValidationError(u"销售类型已设置过")
         return
+
+    def query_type(self, sale_type_id_):
+        if not sale_type_id_:
+            return None
+
+        result = self.env['setting_center.sale_type'].search([('sale_type_id', '=', sale_type_id_)])
+        if result:
+            return result[0].forecast_2_oder_type, \
+                   result[0].order_2_out_store_type, \
+                   result[0].out_store_2_account_type
+
+        return self.env['setting_center'].query_sale_type_2_bill_default()
