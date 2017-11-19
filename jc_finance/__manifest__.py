9245627db00f38c409b281f8b6b4ddd891cc49bf
Eric <xxbregister@126.com>
2017-11-19 21:27:40 +0800

    缺少依赖

--- jc_finance/__manifest__.py
+++ jc_finance/__manifest__.py
@@ -19,7 +19,9 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['archives', 'base'],
+    'depends': ['archives',
+                'decimal_precision',
+                'base'],
 
     # always loaded
     'data': [
