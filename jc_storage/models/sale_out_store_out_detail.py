8dea2661cd62441da7c444a6ef21088a611cd8f8
Eric <xxbregister@126.com>
2017-11-27 16:43:27 +0800

    去掉不必要的字段

--- jc_storage/models/sale_out_store_out_detail.py
+++ jc_storage/models/sale_out_store_out_detail.py
@@ -12,9 +12,9 @@ class SaleOutStoreOutDetail(models.Model):
 
     sale_out_store_id = fields.Many2one('jc_storage.sale_out_store', string='销售出库引用', required=True,
                                         ondelete='cascade', index=True, copy=False)
-
-    source_bill_type = fields.Selection(bill_define.BILL_TYPE, string=u'来源单据类型', readonly=True, copy=False)
-    source_bill_id = fields.Integer(string="来源单据号", readonly=True, copy=False, default=0)
-    source_detail_id = fields.Integer(string="来源单据明细号", readonly=True, copy=False, default=0)
+    #
+    # source_bill_type = fields.Selection(bill_define.BILL_TYPE, string=u'来源单据类型', readonly=True, copy=False)
+    # source_bill_id = fields.Integer(string="来源单据号", readonly=True, copy=False, default=0)
+    # source_detail_id = fields.Integer(string="来源单据明细号", readonly=True, copy=False, default=0)
 
     remark = fields.Char(string=u'备注')
