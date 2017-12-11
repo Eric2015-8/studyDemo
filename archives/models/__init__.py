9608aaee092b09cd8511526877d0cc8d31827d60
Eric <xxbregister@126.com>
2017-12-11 15:31:40 +0800

    修改错误的顺序。否则在创建数据库时报错

--- archives/models/__init__.py
+++ archives/models/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import no_page
-from . import base
-
 from . import bill_define
 from . import department
 from . import staff
@@ -22,5 +19,8 @@ from . import subject
 from . import account
 from . import account_book
 from . import account_book_detail
+
+from . import no_page  # 不能放到goods前面，因为archives.goods_number依赖goods
+from . import base
 
 # from . import approve
